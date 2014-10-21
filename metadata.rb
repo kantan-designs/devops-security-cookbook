d2d84940e0ceadb94f8878269776d797f9dd9a84
John McDowall <john@mcdowall.info>
2014-10-21 12:31:02 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "devops-security-cookbook"
+maintainer        "John McDowall"
+maintainer_email  "john@kantan.io"
+description       "Hardens a basic Ubuntu install by installing UFW, unattended upgrades, Fail2Ban, and firewall rules."
+version           "0.0.2"
+
+recipe "devops-security-cookbook", "Hardens a basic Ubuntu install"
+
+supports "ubuntu"
